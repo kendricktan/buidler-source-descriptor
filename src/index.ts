4cbb9cd226c476f47ce6881c7469ceed2966e812
Kendrick Tan <kendricktan0814@gmail.com>
2020-05-15 14:14:37 +1000

    requires in functions

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,99 @@ import fs from "fs";
 import path from "path";
 import { isObject } from "util";
 
-// Extract information from node types
-const extractFunctionDefNodeInfo = (node: any) => {
+// Just an ad-hoc helper function
+const e2 = (a, b, c) => {
+  try {
+    return a[b][c];
+  } catch (e) {
+    return [];
+  }
+};
+
+// Extracts out lineNumber
+const getLineNumberFromOffset = (solcInput, contractSource, rawOffset) => {
+  if (rawOffset === undefined) {
+    return 0;
+  }
+  // Injects line numbers (retrieved via character offset)
+  const offset = rawOffset.split(":")[0];
+
+  let sourceCode;
+  if (isObject(solcInput.sources[contractSource])) {
+    sourceCode = solcInput.sources[contractSource].content;
+  } else {
+    sourceCode = solcInput.sources[contractSource];
+  }
+
+  const sourceCodeTillOffset = sourceCode.substring(0, parseInt(offset, 10));
+
+  // Line number starts at 1
+  const lineNumber = 1 + (sourceCodeTillOffset.match(/\n/g) || []).length;
+
+  return lineNumber;
+};
+
+// Extract information from node types (This is a partial function...)
+const extractFunctionDefNodeInfo = (solcInput, node) => {
   const { lineNumber, name, visibility } = node;
   const parameters = node.parameters.parameters.map(
-    (x: any) => `${x.typeDescriptions.typeString} ${x.name}`
+    x => `${x.typeDescriptions.typeString} ${x.name}`
   );
   const returnParams = node.returnParameters.parameters.map(
-    (x: any) => `${x.typeDescriptions.typeString}`
+    x => `${x.typeDescriptions.typeString}`
   );
-  const modifiers = node.modifiers.map((x: any) => x.modifierName.name);
+  const modifiers = node.modifiers.map(x => x.modifierName.name);
   const events =
     node.body === null
       ? []
       : node.body.statements
-          .filter((x: any) => x.nodeType === "EmitStatement")
-          .map((x: any) => x.eventCall.expression.name);
+          .filter(x => x.nodeType === "EmitStatement")
+          .map(x => x.eventCall.expression.name);
+
+  const requires =
+    node.body === null
+      ? []
+      : node.body.statements
+          .filter(
+            x =>
+              x.nodeType === "ExpressionStatement" ||
+              x.nodeType === "FunctionCall"
+          )
+          .map(x => x.expression)
+          .filter(x => {
+            try {
+              return x.expression.name.toLowerCase().includes("require");
+            } catch {
+              return false;
+            }
+          })
+          .map(x => {
+            const reqLineNumber = getLineNumberFromOffset(
+              solcInput,
+              node.contractSource,
+              x.src
+            );
+
+            if (x.expression.name === "require") {
+              const literalString = x.expression.argumentTypes
+                .slice(-1)[0]
+                .typeString.split(" ")
+                .slice(1)
+                .join(" ")
+                .split('"')
+                .join("");
+
+              return {
+                lineNumber: reqLineNumber,
+                name: `require(..., ${literalString})`
+              };
+            }
+
+            return {
+              lineNumber: reqLineNumber,
+              name: x.expression.name
+            };
+          });
 
   return {
     name: name === "" ? node.kind : name,
@@ -27,6 +104,7 @@ const extractFunctionDefNodeInfo = (node: any) => {
     events,
     modifiers,
     visibility,
+    requires,
     lineNumber
   };
 };
@@ -142,27 +220,11 @@ task("build:ast-doc", "Generate document representation (in JSON) from AST")
             return { contractSource, contractName, ...x };
           })
           .map(x => {
-            if (x.src === undefined) {
-              return x;
-            }
-            // Injects line numbers (retrieved via character offset)
-            const offset = x.src.split(":")[0];
-
-            let sourceCode;
-            if (isObject(solcInput.sources[x.contractSource])) {
-              sourceCode = solcInput.sources[x.contractSource].content;
-            } else {
-              sourceCode = solcInput.sources[x.contractSource];
-            }
-
-            const sourceCodeTillOffset = sourceCode.substring(
-              0,
-              parseInt(offset, 10)
+            const lineNumber = getLineNumberFromOffset(
+              solcInput,
+              x.contractSource,
+              x.src
             );
-
-            // Line number starts at 1
-            const lineNumber =
-              1 + (sourceCodeTillOffset.match(/\n/g) || []).length;
 
             return { lineNumber, ...x };
           });
@@ -221,9 +283,8 @@ task("build:ast-doc", "Generate document representation (in JSON) from AST")
       }, {});
     };
 
-    const functionsInContracts = massageToInContracts(
-      functionDefs,
-      extractFunctionDefNodeInfo
+    const functionsInContracts = massageToInContracts(functionDefs, node =>
+      extractFunctionDefNodeInfo(solcInput, node)
     );
     const eventsInContracts = massageToInContracts(
       eventDefs,
@@ -253,24 +314,15 @@ task("build:ast-doc", "Generate document representation (in JSON) from AST")
           ...structsInContracts[x]
         });
 
-        // Just an ad-hoc helper function
-        const hf = (a, b, c) => {
-          try {
-            return a[b][c];
-          } catch (e) {
-            return [];
-          }
-        };
-
         const contractWithContext = contractNames
           .map(y => {
             return {
               [y]: {
-                functions: hf(functionsInContracts, x, y),
-                events: hf(eventsInContracts, x, y),
-                variables: hf(variablesInContracts, x, y),
-                modifiers: hf(modifiersInContracts, x, y),
-                structs: hf(structsInContracts, x, y)
+                functions: e2(functionsInContracts, x, y),
+                events: e2(eventsInContracts, x, y),
+                variables: e2(variablesInContracts, x, y),
+                modifiers: e2(modifiersInContracts, x, y),
+                structs: e2(structsInContracts, x, y)
               }
             };
           })
@@ -286,7 +338,8 @@ task("build:ast-doc", "Generate document representation (in JSON) from AST")
         return { ...acc, ...x };
       }, {});
 
-    // Inject metadata into the formatted ast doc output
+    // Massage the output to a more user friendly format
+    // And also inject in the imports
     // e.g. all contracts will now be in contracts key
     const astDocOutputFixed1 = Object.keys(astDocOutput)
       .map(x => {
@@ -305,7 +358,7 @@ task("build:ast-doc", "Generate document representation (in JSON) from AST")
         return { ...acc, ...x };
       }, {});
 
-    // Injects in inheritance for each contract
+    // Injects in inheritance and imports for each contract
     const astDocOutputFixed2 = Object.keys(astDocOutputFixed1)
       .map(x => {
         // Get all inheritance for each contract
--- test/buidler-project/contracts/Child.sol
+++ test/buidler-project/contracts/Child.sol
@@ -2,8 +2,27 @@ pragma solidity ^0.5.0;
 
 import "./Parent.sol";
 
+
 contract Child is Parent {
+    function _fWithRequire() internal {
+        require(true, "False is true");
+    }
+
+    function _fWithRequireArgs(uint i) internal {
+        require(i > 0 && i <= 5, "i not in range");
+    }
+
     function myFunction() external onlyOwner {
+        _fWithRequire();
+        _fWithRequireArgs(10);
+        require(0 < 1, "0 is greater than 1");
+
+        uint256 i = 1 + 2;
+        uint256 i2 = i + 3;
+        uint256 i3 = i2 + 5;
+
+        i3 = 54;
+
         emit OwnerNominated(msg.sender);
     }
 }
