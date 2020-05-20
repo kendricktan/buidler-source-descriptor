07c587ed37dbd2cefd8343c33cf07165b335c48f
justin j. moses <justinjmoses@gmail.com>
2020-05-20 11:08:03 -0400

    Moving AST docs to build step, adding path, file and ignores config options

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,117 @@
 import { extendEnvironment, task } from "@nomiclabs/buidler/config";
+import { BuidlerPluginError, lazyObject } from "@nomiclabs/buidler/plugins";
+import { SOLC_INPUT_FILENAME, SOLC_OUTPUT_FILENAME } from "@nomiclabs/buidler/internal/constants";
+
 import fs from "fs";
 import path from "path";
 
 import { extractASTInformation } from "./helpers";
 
+export class ASTDocsBuidlerEnvironment {
+  constructor(
+    public readonly path: string = undefined,
+    public readonly file: string = "ast-docs.json",
+    public readonly ignores: string = ""
+  ) { }
+}
+
+declare module "@nomiclabs/buidler/types" {
+  export interface BuidlerRuntimeEnvironment {
+    astdocs: ASTDocsBuidlerEnvironment;
+  }
+
+  export interface BuidlerConfig {
+    astdocs?: {
+      path?: string;
+      file?: string;
+      ignores?: string;
+    };
+  }
+}
+
 // Everything in a plugin must happen inside an exported function
 extendEnvironment((env: any) => {
   // Force env to generate AST
   env.config.solc.outputSelection = { "*": { "*": ["*"], "": ["*"] } };
+
+  env.astdocs = lazyObject(() => {
+    if (env.config.astdocs) {
+      return new ASTDocsBuidlerEnvironment(
+        env.config.astdocs.path,
+        env.config.astdocs.file,
+        env.config.astdocs.ignores
+      );
+    }
+    return new ASTDocsBuidlerEnvironment();
+  });
 });
 
-// Add a new task
-task("build:ast-doc", "Generate document representation (in JSON) from AST")
-  .addParam(
-    "astDocDir",
-    "Directory to output document representation extracted from the AST"
-  )
-  .setAction(async (taskArguments, bre) => {
-    const { astDocDir } = taskArguments;
+task("compile", "compilation step", async (taskArguments, bre, runSuper) => {
+  await runSuper();
 
-    const cachePath = bre.config.paths.cache;
-    const solcInput = require(path.resolve(cachePath, "solc-input.json"));
-    const solcOutput = require(path.resolve(cachePath, "solc-output.json"));
+  // now do AST processing
+  createAST({ bre });
+});
 
-    const contractInformation = extractASTInformation(
-      solcInput,
-      solcOutput,
-      "contract"
-    );
-    const interfaceInformation = extractASTInformation(
-      solcInput,
-      solcOutput,
-      "interface"
-    );
-    const libraryInformation = extractASTInformation(
-      solcInput,
-      solcOutput,
-      "library"
-    );
+const createAST = ({ bre }) => {
+  const { astdocs }: { astdocs: ASTDocsBuidlerEnvironment } = bre;
+  const cachePath = bre.config.paths.cache;
+  const astDocDir = astdocs.path || cachePath;
 
-    const astDocsData = Object.keys(solcInput.sources)
-      .map(x => {
-        const imports = solcOutput.sources[x].ast.nodes
-          .filter(n => n.nodeType === "ImportDirective")
-          .map(n => n.absolutePath);
+  const solcInputPath = path.resolve(cachePath, SOLC_INPUT_FILENAME);
+  const solcOutputPath = path.resolve(cachePath, SOLC_OUTPUT_FILENAME);
 
-        return {
-          [x]: {
-            imports,
-            contracts: { ...contractInformation[x].contract },
-            interfaces: { ...interfaceInformation[x].interface },
-            libraries: { ...libraryInformation[x].library }
-          }
-        };
-      })
-      .reduce((acc, x) => {
-        return { ...acc, ...x };
-      }, {});
+  if (!fs.existsSync(solcInputPath)) {
+    throw new BuidlerPluginError("Cannot find solidity input source");
+  } else if (!fs.existsSync(solcOutputPath)) {
+    throw new BuidlerPluginError("Cannot find solidity output source");
+  }
+  const solcInput = require(solcInputPath);
+  const solcOutput = require(solcOutputPath);
 
-    // Write to file
-    if (!fs.existsSync(astDocDir)) {
-      fs.mkdirSync(astDocDir);
-    }
-    fs.writeFileSync(
-      path.resolve(astDocDir, "ast-docs.json"),
-      JSON.stringify(astDocsData, null, 4)
-    );
-  });
+  const contractInformation = extractASTInformation(
+    solcInput,
+    solcOutput,
+    "contract"
+  );
+  const interfaceInformation = extractASTInformation(
+    solcInput,
+    solcOutput,
+    "interface"
+  );
+  const libraryInformation = extractASTInformation(
+    solcInput,
+    solcOutput,
+    "library"
+  );
+
+  const astDocsData = Object.keys(solcInput.sources)
+    .filter(x => !astdocs.ignores || !x.includes(astdocs.ignores))
+    .map(x => {
+      const imports = solcOutput.sources[x].ast.nodes
+        .filter(n => n.nodeType === "ImportDirective")
+        .map(n => n.absolutePath);
+
+      return {
+        [x]: {
+          imports,
+          contracts: { ...contractInformation[x].contract },
+          interfaces: { ...interfaceInformation[x].interface },
+          libraries: { ...libraryInformation[x].library }
+        }
+      };
+    })
+    .reduce((acc, x) => {
+      return { ...acc, ...x };
+    }, {});
+
+  // Write to file
+  if (!fs.existsSync(astDocDir)) {
+    fs.mkdirSync(astDocDir);
+  }
+
+  fs.writeFileSync(
+    path.resolve(astDocDir, astdocs.file),
+    JSON.stringify(astDocsData, null, 4)
+  );
+}
